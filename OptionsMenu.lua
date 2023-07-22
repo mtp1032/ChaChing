f31e61d5d58f20df9f5a4a1c8736c2053be9ed59
Michael Peterson <mtpeterson1948@gmail.com>
2023-07-21 20:26:39 -0600

    Update OptionsMenu.lua
    Rewrote createBagIcon()
    Eliminated redundant call to createOptionsPanel()
    Added Checkbox ability to all bag icons

--- OptionsMenu.lua
+++ OptionsMenu.lua
@@ -16,6 +16,8 @@ local EMPTY_STR 		= core.EMPTY_STR
 local sellGrey 	= true
 local sellWhite = false
 
+local sellAll = false
+
 local GreyQualityButton = nil
 local WhiteQualityButton = nil
 
@@ -29,6 +31,9 @@ local FRAME_HEIGHT = 700
 local WIDTH_TITLE_BAR = 500
 local HEIGHT_TITLE_BAR = 45
 
+local BUTTON_WIDTH  = 48
+local BUTTON_HEIGHT = 48
+
 local function drawLine( yPos, f)
 	local lineFrame = CreateFrame("FRAME", nil, f )
 	lineFrame:SetPoint("CENTER", -10, yPos )
@@ -40,55 +45,6 @@ local function drawLine( yPos, f)
 	line:SetStartPoint("LEFT",X_START_POINT, Y_START_POINT)
 	line:SetEndPoint("RIGHT", X_START_POINT, Y_START_POINT)
 	lineFrame:Show() 
-end
-local function createBagIcon( f, bagName, iconFileId, bagSlot )
-	local BUTTON_WIDTH  = 48
-	local BUTTON_HEIGHT = 48
-
-	local bagIcon = CreateFrame("Button","CHACHING_BagButton", f,"TooltipBackdropTemplate")
-
-	bagIcon.BagName = bagName
-	bagIcon.BagSlot = bagSlot
-	bagIcon.BagIndex = bagSlot + 1
-
-	bagIcon.tooltip = L["BAG_TOOLTIP"]
-	local freeSlots = C_Container.GetContainerNumFreeSlots( bagSlot)
-	bagIcon.Lable = sprintf("%s: %d free slots.", bagName, freeSlots )
-
-	bagIcon:SetSize( BUTTON_WIDTH, BUTTON_HEIGHT )
-
-		-- Position bag buttons icons vertically
-	bagIcon.xPos = 60
-	bagIcon.yPos = 150 - (BUTTON_HEIGHT + 10) * ( bagIcon.BagIndex )
-	bagIcon:SetPoint( "LEFT", bagIcon.xPos, bagIcon.yPos )
-
-
-	local message = f:CreateFontString(nil, "ARTWORK","GameFontNormal")
-	message:SetJustifyH("LEFT")
-	message:SetPoint("LEFT", bagIcon.xPos + (BUTTON_WIDTH + 20), bagIcon.yPos )
-	message:SetText( bagIcon.Lable )
-
-	bagIcon.Texture = bagIcon:CreateTexture(nil,"ARTWORK")
-	bagIcon.Texture:SetPoint("TOPLEFT",3,-3)
-	bagIcon.Texture:SetPoint("BOTTOMRIGHT",-3,3)
-	bagIcon.Texture:SetTexCoord(0.075,0.925,0.075,0.925) -- trim off icon's edges
-
-	bagIcon:SetNormalTexture(iconFileId)
-	bagIcon:SetHighlightTexture(iconFileId )
-	bagIcon:GetHighlightTexture():SetAlpha(0.8)
-
-	bagIcon.Mask = bagIcon:CreateMaskTexture(nil,"ARTWORK")
-	bagIcon.Mask:SetPoint("TOPLEFT",bagIcon.Texture,"TOPLEFT")
-	bagIcon.Mask:SetPoint("BOTTOMRIGHT",bagIcon.Texture,"BOTTOMRIGHT")
-	bagIcon.Mask:SetTexture("Interface\\Common\\common-iconmask.blp")
-	bagIcon.Texture:AddMaskTexture(bagIcon.Mask)
-
-	bagIcon:RegisterForClicks("AnyDown")
-	bagIcon:SetScript("OnClick", 
-	function ( self )
-		item:setBagChecked( bagIcon.BagSlot )
-	end)
-	return bagIcon
 end
 local function showGreyWhiteCheckBoxes( frame, yPos )
 
@@ -117,51 +73,138 @@ local function showGreyWhiteCheckBoxes( frame, yPos )
 			CHACHING_SAVED_OPTIONS.sellWhite = self:GetChecked() and true or false
 			item:setWhiteChecked( CHACHING_SAVED_OPTIONS.sellWhite )
     	end)
-	end -- end of OnClick
-local function createOptionsPanel()
-	if optionsPanel ~= nil then
-		return optionsPanel
+end -- end of OnClick
+
+local function createBagCheckBox( frame, bagSlot, xPos, yPos )
+    checkBackpack = CreateFrame("CheckButton", "CHACHING_checkBackpack", frame, "ChatConfigCheckButtonTemplate")
+	xPos = xPos - 10
+	yPos = (yPos)/50
+	checkBackpack:SetPoint("LEFT", xPos, yPos )
+	checkBackpack:SetChecked( sellAll )
+	checkBackpack:SetScript("OnClick", 
+		function(self )
+			local bagName = C_Container.GetBagName( bagSlot )
+			local sellAll = self:GetChecked() and true or false
+			if sellAll then
+				item:setBagChecked( bagName, bagSlot )
+			else
+				item:setBagUnchecked( bagName, bagSlot )
+			end
+		end)
+end -- end of OnClick
+
+local function createBagIcon( f, bagName, caption, bagSlot )
+
+	local iconFrame = CreateFrame("Button","CHACHING_BagButton", f,"TooltipBackdropTemplate")
+
+	iconFrame.BagName 		= bagName
+	iconFrame.BagSlot 		= bagSlot
+	iconFrame.BagIndex 		= bagSlot + 1
+	iconFrame.NumFreeSlots	= C_Container.GetContainerNumFreeSlots(bagSlot)
+	iconFrame.Caption 		= caption
+
+	if bagSlot == 0 then
+		iconFrame.IconFileId = "bag-main"
+	else
+		iconFrame.IconFileId = GetItemIcon( iconFrame.BagName )
 	end
+
+	iconFrame.tooltip = L["BAG_TOOLTIP"]
+	iconFrame:SetSize( BUTTON_WIDTH, BUTTON_HEIGHT )
+
+		-- Position bag buttons icons vertically
+	iconFrame.xPos = 60
+	iconFrame.yPos = 120 - (BUTTON_HEIGHT + 10) * ( iconFrame.BagIndex )
+
+	-- This positions the bag icon.
+	iconFrame:SetPoint( "LEFT", iconFrame.xPos, iconFrame.yPos )
+	createBagCheckBox( iconFrame, iconFrame.BagSlot, iconFrame.xPos, iconFrame.yPos )
+
+	local msg = f:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+	msg:SetJustifyH("LEFT")
+	-- This positions the icon text
+	msg:SetPoint("LEFT", iconFrame.xPos + (BUTTON_WIDTH + 30), iconFrame.yPos )
+	msg:SetText( iconFrame.Caption )
+	iconFrame.message = msg
+
+	iconFrame.Texture = iconFrame:CreateTexture(nil,"ARTWORK")
+	iconFrame.Texture:SetPoint("TOPLEFT",3,-3)
+	iconFrame.Texture:SetPoint("BOTTOMRIGHT",-3,3)
+	iconFrame.Texture:SetTexCoord(0.075,0.925,0.075,0.925) -- trim off icon's edges
+
+	iconFrame:SetNormalTexture(iconFrame.IconFileId)
+	iconFrame:SetHighlightTexture(iconFrame.IconFileId )
+	iconFrame:GetHighlightTexture():SetAlpha(0.8)
+
+	iconFrame.Mask = iconFrame:CreateMaskTexture(nil,"ARTWORK")
+	iconFrame.Mask:SetPoint("TOPLEFT",iconFrame.Texture,"TOPLEFT")
+	iconFrame.Mask:SetPoint("BOTTOMRIGHT",iconFrame.Texture,"BOTTOMRIGHT")
+	iconFrame.Mask:SetTexture("Interface\\Common\\common-iconmask.blp")
+	iconFrame.Texture:AddMaskTexture(iconFrame.Mask)
+
+	-- iconFrame:RegisterForClicks("LeftButtonDown", "RightButtonDown" )
+	iconFrame:SetScript("OnClick", 
+	function ( self, iconFrame )
+		if iconFrame == "LeftButton" and not IsShiftKeyDown() then
+			print( dbg:prefix(), bagSlot )
+			item:setBagChecked( bagSlot )
+		end
+		if iconFrame == "RightButton" and not IsShiftKeyDown() then
+			print( dbg:prefix(), bagSlot )
+			item:clearBagChecked( bagSlot )
+		end
+		if iconFrame == "LeftButton" and IsShiftKeyDown() then
+			print( dbg:prefix(), iconFrame.BagSlot )
+			item:uncheckAllBags( bagSlot )
+		end
+		if iconFrame == "RightButton" and IsShiftKeyDown() then
+			print( dbg:prefix(), bagSlot )
+			item:clearBagChecked( bagSlot )
+		end
+	end)
+	return iconFrame
+end
+local function createOptionsPanel()
 		
 	local frame = CreateFrame("Frame", L["OPTIONS_MENU_TITLE"], UIParent, BackdropTemplateMixin and "BackdropTemplate")
 	frame:SetFrameStrata("HIGH")
 	frame:SetToplevel(true)
 	frame:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
-    frame:EnableMouse(true)
-    frame:EnableMouseWheel(true)
-    frame:SetMovable(true)
-    frame:RegisterForDrag("LeftButton")
-    frame:SetScript("OnDragStart", frame.StartMoving)
-    frame:SetScript("OnDragStop", frame.StopMovingOrSizing)
+	frame:EnableMouse(true)
+	frame:EnableMouseWheel(true)
+	frame:SetMovable(true)
+	frame:RegisterForDrag("LeftButton")
+	frame:SetScript("OnDragStart", frame.StartMoving)
+	frame:SetScript("OnDragStop", frame.StopMovingOrSizing)
 	frame:SetBackdrop({
-        bgFile = "Interface\\ChatFrame\\ChatFrameBackground",
-        edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
-        edgeSize = 26,
-        insets = {left = 9, right = 9, top = 9, bottom = 9},
-    })
-    frame:SetBackdropColor(0.0, 0.0, 0.0, 0.80)
+		bgFile = "Interface\\ChatFrame\\ChatFrameBackground",
+		edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
+		edgeSize = 26,
+		insets = {left = 9, right = 9, top = 9, bottom = 9},
+	})
+	frame:SetBackdropColor(0.0, 0.0, 0.0, 0.80)
 
-    -- The Title Bar & Title
+	-- The Title Bar & Title
 	frame.titleBar = frame:CreateTexture(nil, "ARTWORK")
 	frame.titleBar:SetTexture("Interface\\DialogFrame\\UI-DialogBox-Header")
 	frame.titleBar:SetPoint("TOP", 0, 12)
-    frame.titleBar:SetSize( WIDTH_TITLE_BAR, HEIGHT_TITLE_BAR)
+	frame.titleBar:SetSize( WIDTH_TITLE_BAR, HEIGHT_TITLE_BAR)
 
 	frame.title = frame:CreateFontString(nil, "ARTWORK", "GameFontNormal")
 	frame.title:SetPoint("TOP", 0, 4)
 	frame.title:SetText("ChaChing Options Menu")
 
-    -- Title text
+	-- Title text
 	frame.text = frame:CreateFontString(nil, "ARTWORK", "GameFontHighlight")
 	frame.text:SetPoint("TOPLEFT", 12, -22)
 	frame.text:SetWidth(frame:GetWidth() - 20)
 	frame.text:SetJustifyH("LEFT")
 	frame:SetHeight(frame.text:GetHeight() + 70)
 	tinsert( UISpecialFrames, frame:GetName() )
-    frame:SetSize( FRAME_WIDTH, FRAME_HEIGHT )
+	frame:SetSize( FRAME_WIDTH, FRAME_HEIGHT )
 
 	-------------------- INTRO HEADER -----------------------------------------
-    local subTitle = frame:CreateFontString(nil, "ARTWORK","GameFontNormalLarge")
+	local subTitle = frame:CreateFontString(nil, "ARTWORK","GameFontNormalLarge")
 	local msgText = frame:CreateFontString(nil, "ARTWORK","GameFontNormalLarge")
 	local displayString = sprintf("%s", L["ADDON_NAME_AND_VERSION"] )
 	msgText:SetPoint("TOP", 0, -20)
@@ -176,41 +219,39 @@ local function createOptionsPanel()
 	messageText:SetJustifyH("LEFT")
 	messageText:SetPoint("CENTER", 0, 150)
 	messageText:SetText( msgText )
+	frame.messageText = messageText
 
-    -------------------- WARNING DESCRIPTION ---------------------------------------
+	-------------------- WARNING DESCRIPTION ---------------------------------------
 
 	showGreyWhiteCheckBoxes( frame, -80 )
 	-- drawLine( 150, frame )
 
-	frame.BagIcons = {}
-	local bagIndex = 1
-	-- bag-main = MainMenuBarBackpackButton:GetSlotAtlases()
-	frame.BagIcons[bagIndex] = createBagIcon( frame, "Player's Backpack", "bag-main", 0  )
-
-	for bagIndex = 2, 5 do
-		local bagSlot = bagIndex - 1
-		local bagName = C_Container.GetBagName( bagSlot )
+	frame.BagIcons = {}	
+	for i = 0, 4 do
+		local bagName = C_Container.GetBagName( i )
 		if bagName ~= nil then
-			local iconFileId = GetItemIcon( bagName )
-			frame.BagIcons[bagIndex] = createBagIcon( frame, bagName, iconFileId, bagSlot  )
+			local freeSlots = C_Container.GetContainerNumFreeSlots( i )
+			local caption = sprintf("%s: %d available slots.", bagName, freeSlots )
+			local iconFrame = createBagIcon( frame, bagName, caption, i  )
+			frame.BagIcons[i + 1] = iconFrame
 		end
 	end
 
-    -- Defaults buttom, bottom left
+	-- Defaults buttom, bottom left
 	frame.hide = CreateFrame("Button", nil, frame, "UIPanelButtonTemplate")
 	frame.hide:SetText("Default")
 	frame.hide:SetHeight(20)
 	frame.hide:SetWidth(80)
 	frame.hide:SetPoint("BOTTOMLEFT", frame, "BOTTOMLEFT", 8, 8)
 	frame.hide:SetScript("OnClick",         
-    	function(self)
+		function(self)
 			CHACHING_SAVED_OPTIONS.sellGrey = true
 			CHACHING_SAVED_OPTIONS.sellWhite = false
-			item:initBagTable()
+			item:setBagDefaults()
 			frame:Hide()
-    end)
+	end)
 
-    -- Accept buttom, bottom right
+	-- Accept buttom, bottom right
 	frame.hide = CreateFrame("Button", nil, frame, "UIPanelButtonTemplate")
 	frame.hide:SetText("Accept")
 	frame.hide:SetHeight(20)
@@ -218,26 +259,19 @@ local function createOptionsPanel()
 	frame.hide:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT", -8, 8)
 	frame.hide:SetScript("OnClick",
 		function( self )
-			for bagSlot = 0, 4 do
-				local bagName = C_Container.GetBagName( bagSlot )
-				if bagName ~= nil then
-					local freeSlots = C_Container.GetContainerNumFreeSlots( bagSlot )
-					frame.BagIcons[bagSlot+1].Lable = sprintf("%s has %d free slots.", bagName, freeSlots )
-		 			-- print( dbg:prefix(), frame.BagIcons[bagSlot+1].Lable )
-				end
-			end
 			frame:Hide()
 		end)
 
-    -- Create the Exclusion List Input Edit Box
+	-- Create the Exclusion List Input Edit Box
 	local DescrSubHeader = frame:CreateFontString(nil, "ARTWORK","GameFontNormalLarge")
-    DescrSubHeader:SetPoint("CENTER", 0, -275 )
+	DescrSubHeader:SetPoint("LEFT", 60, -250 )
 	DescrSubHeader:SetText("Excluded Item List" )
 	local f = CreateFrame("EditBox", "InputEditBox", frame, "InputBoxTemplate")
 	f:SetFrameStrata("DIALOG")
 	f:SetSize(200,75)
 	f:SetAutoFocus(false)
-	f:SetPoint("CENTER", 0, -250) 
+	-- f.tooltip = sprintf("Drag and drop items to be excluded into the Edit Box.")
+	f:SetPoint("CENTER", 25, -250) 
 	f:SetScript("OnMouseUp", 
 		function(self,button)
 			cursorInfo, itemId, itemLink = GetCursorInfo()
@@ -258,8 +292,27 @@ end
 local optionsPanel = createOptionsPanel()
 
 function options:showOptionsPanel()
+	if optionsPanel == nil then
+		optionsPanal = createOptionsPanel()
+	end
+
 	sellGrey = CHACHING_SAVED_OPTIONS.sellGrey
 	sellWhite = CHACHING_SAVED_OPTIONS.sellWhite
+
+	for i = 0, 4 do
+		local bagSlot = i
+		local bagName = C_Container.GetBagName( i )
+		if bagName ~= nil then
+			local freeSlots = C_Container.GetContainerNumFreeSlots(i)
+			local caption = sprintf("%s: %d available slots.", bagName, freeSlots)
+			optionsPanel.BagIcons[i+1].message:Hide()
+			optionsPanel.BagIcons[i+1].message:SetText( caption )
+			optionsPanel.BagIcons[i+1].message:Show()
+			local xPos = optionsPanel.BagIcons[i+1].xPos
+			local yPos = optionsPanel.BagIcons[i+1].yPos
+			print( dbg:prefix(), sprintf("%s: xPos %d, yPos %d", bagName, xPos, yPos ))
+		end
+	end
 	optionsPanel:Show()
 end
 function options:hideOptionsPanel()
@@ -268,7 +321,7 @@ end
 
 local eventFrame = CreateFrame("Frame" )
 eventFrame:RegisterEvent("BAG_UPDATE")				-- bagID
-
+eventFrame:RegisterEvent("MERCHANT_CLOSED")
 eventFrame:SetScript("OnEvent", 
 function( self, event, ... )
 	local arg1 = ...
@@ -276,11 +329,15 @@ function( self, event, ... )
 	-- and once when dropped.
 	if event == "BAG_UPDATE" then
 		local bagSlot = ...
-		local bagName = C_Container.GetBagName( bagSlot )
-		local freeSlots = C_Container.GetContainerNumFreeSlots( bagSlot )
-		optionsPanel.BagIcons[bagSlot +1].Lable = sprintf("[%s] %s updated - has %d free slots.", event, bagName, freeSlots) 
-		-- print( dbg:prefix(), optionsPanel.BagIcons[bagSlot +1].Lable)
-		DEFAULT_CHAT_FRAME:AddMessage( sprintf("[%s] %s updated - has %d free slots.", event, bagName, freeSlots), 0, 1, 0)
+		for bagSlot = 0, 4 do
+			local bagName = C_Container.GetBagName( bagSlot )
+			if bagName ~= nil then
+				-- DEFAULT_CHAT_FRAME:AddMessage( sprintf("[%s] %s updated.", event, bagName ), 0, 1, 0)
+			end
+		end
+	end
+	if event == "MERCHANT_CLOSED" then
+		optionsPanel:Hide()
 	end
 end)
 
@@ -288,3 +345,7 @@ local fileName = "OptionsMenu.lua"
 if core:debuggingIsEnabled() then
 	DEFAULT_CHAT_FRAME:AddMessage( sprintf("%s loaded", fileName), 1.0, 1.0, 0.0 )
 end
+
+
+
+
